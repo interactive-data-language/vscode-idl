b80f24ce6c9f5f6e2e409e44d20956e424c71663
Zachary Norman <zachary.norman@nv5.com>
2023-07-03 16:50:00 -0600

    Make log debug

--- libs/vscode/server/src/lib/user-interaction/events/on-code-action.ts
+++ libs/vscode/server/src/lib/user-interaction/events/on-code-action.ts
@@ -15,7 +15,7 @@ export const ON_CODE_ACTIONS = async (
   try {
     IDL_LANGUAGE_SERVER_LOGGER.log({
       log: IDL_LSP_LOG,
-      type: 'info',
+      type: 'debug',
       content: ['CodeAction request', params],
     });
 
