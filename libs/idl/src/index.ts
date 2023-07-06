95eb6cd8c28dd2754a53f05ffe49adc4a6d2d912
Zachary Norman <zachary.norman@nv5.com>
2023-07-05 21:34:49 -0600

    Remove duplicate import

--- libs/idl/src/index.ts
+++ libs/idl/src/index.ts
@@ -2,5 +2,4 @@ export * from './lib/events.interface';
 export * from './lib/helpers/clean-idl-output';
 export * from './lib/idl.class';
 export * from './lib/idl.interface';
-export * from './lib/idl.interface';
 export * from './lib/utils/regex';
