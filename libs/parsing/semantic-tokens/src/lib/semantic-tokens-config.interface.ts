51a098792f9e50839bdcc89d712be2e90e7a141c
Zachary Norman <zachary.norman@nv5.com>
2023-07-08 12:14:37 -0600

    Use lookup for semantic tokens

--- libs/parsing/semantic-tokens/src/lib/semantic-tokens-config.interface.ts
+++ libs/parsing/semantic-tokens/src/lib/semantic-tokens-config.interface.ts
@@ -12,11 +12,32 @@ import {
 export const SEMANTIC_TOKEN_TYPES = [SemanticTokenTypes.class];
 
 /**
+ * Lookup to map the types of semantic tokens from their names to array elements
+ */
+export const SEMANTIC_TOKEN_TYPE_INDEX_LOOKUP: { [key: string]: number } = {};
+
+// populate
+for (let i = 0; i < SEMANTIC_TOKEN_TYPES.length; i++) {
+  SEMANTIC_TOKEN_TYPE_INDEX_LOOKUP[SEMANTIC_TOKEN_TYPES[i]] = i;
+}
+
+/**
  * Modifiers for semantic tokens that we extract
  *
  * We can only send modifiers that we capture here
  */
 export const SEMANTIC_TOKEN_MODIFIERS = [SemanticTokenModifiers.definition];
+
+/**
+ * Lookup to map the modifiers from their names to array elements
+ */
+export const SEMANTIC_TOKEN_MODIFIER_INDEX_LOOKUP: { [key: string]: number } =
+  {};
+
+// populate
+for (let i = 0; i < SEMANTIC_TOKEN_MODIFIERS.length; i++) {
+  SEMANTIC_TOKEN_MODIFIER_INDEX_LOOKUP[SEMANTIC_TOKEN_MODIFIERS[i]] = i;
+}
 
 /**
  * Legend for semantic tokens
