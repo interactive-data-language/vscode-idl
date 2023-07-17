c340851a2dddbf5d2323af32a1ab21d672ee14ca
Zachary Norman <zachary.norman@nv5.com>
2023-07-17 00:07:05 -0600

    Tie in hover help for notebooks

--- libs/vscode/notebooks/src/lib/providers/register-notebook-hover-provider.ts
+++ libs/vscode/notebooks/src/lib/providers/register-notebook-hover-provider.ts
@@ -1,4 +1,9 @@
+import { LANGUAGE_SERVER_CLIENT } from '@idl/vscode/client';
 import * as vscode from 'vscode';
+import {
+  Hover as LanguageServerHover,
+  TextDocumentPositionParams,
+} from 'vscode-languageserver';
 
 import { GetNotebookCellIndex } from '../helpers/get-notebook-cell-index';
 import { IDL_NOTEBOOK_SELECTION_SCHEME } from './providers.interface';
@@ -8,26 +13,57 @@ import { IDL_NOTEBOOK_SELECTION_SCHEME } from './providers.interface';
  */
 export function RegisterNotebookHoverProvider() {
   vscode.languages.registerHoverProvider(IDL_NOTEBOOK_SELECTION_SCHEME, {
-    provideHover(document, position, _token) {
+    async provideHover(document, position, _token) {
+      /**
+       * Get notebook and document for index
+       */
       const doc = GetNotebookCellIndex(document);
 
+      // return if no matching doc
       if (doc === undefined) {
         return undefined;
       }
 
-      console.log({
-        uri: doc.notebook.uri.toString(),
-        cell: doc.cell,
-        line: position.line,
-        character: position.character,
-      });
+      /**
+       * Create hover help parameters - typed for strictness
+       */
+      const params: TextDocumentPositionParams = {
+        textDocument: {
+          uri: `${doc.notebook.uri.toString()}#${doc.cell}`,
+        },
+        position: {
+          line: position.line,
+          character: position.character,
+        },
+      };
 
-      // get hover help
-      // LANGUAGE_SERVER_CLIENT.sendRequest()
+      /**
+       * Send and wait for a response from the server
+       */
+      const hovered: LanguageServerHover =
+        await LANGUAGE_SERVER_CLIENT.sendRequest('textDocument/hover', params);
 
-      // return sample hover
-      // return new Hover('I am a hover!');
-      return undefined;
+      /**
+       * Init range for hover help
+       */
+      let range: vscode.Range;
+
+      // populate if we have a range property
+      if (hovered?.range !== undefined) {
+        range = new vscode.Range(
+          new vscode.Position(
+            hovered.range.start.line,
+            hovered.range.start.character
+          ),
+          new vscode.Position(
+            hovered.range.end.line,
+            hovered.range.end.character
+          )
+        );
+      }
+
+      // convert to VSCode hover help
+      return new vscode.Hover(hovered.contents as string, range);
     },
   });
 }
