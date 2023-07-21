bfbc6a0f263f9bd26f20f98df470bd140f4c9488
Zachary Norman <zachary.norman@nv5.com>
2023-07-21 14:23:57 -0600

    Main thread filter

--- libs/parsing/index/src/lib/global-index.class.ts
+++ libs/parsing/index/src/lib/global-index.class.ts
@@ -282,23 +282,25 @@ export class GlobalIndex {
           // check if we match
           if (byName[z].file === file) {
             // decide how to proceed
-            switch (true) {
-              // 3 or more duplicates
-              case byName.length > 2:
-                this.removeDuplicateTokenProblems(byName[z], false);
-                break;
-              // if we have two, and we remove one, all problems must leave
-              case byName.length == 2:
-                this.removeDuplicateTokenProblems(byName[0], true);
-                this.removeDuplicateTokenProblems(byName[1], true);
-                break;
-              case byName.length === 1:
-                // remove display name
-                delete IDL_DISPLAY_NAMES[tokens[i].type][tokens[i].name];
-                break;
-              default:
-                // do nothing
-                break;
+            if (IDL_INDEX_OPTIONS.IS_MAIN_THREAD) {
+              switch (true) {
+                // 3 or more duplicates
+                case byName.length > 2:
+                  this.removeDuplicateTokenProblems(byName[z], false);
+                  break;
+                // if we have two, and we remove one, all problems must leave
+                case byName.length == 2:
+                  this.removeDuplicateTokenProblems(byName[0], true);
+                  this.removeDuplicateTokenProblems(byName[1], true);
+                  break;
+                case byName.length === 1:
+                  // remove display name
+                  delete IDL_DISPLAY_NAMES[tokens[i].type][tokens[i].name];
+                  break;
+                default:
+                  // do nothing
+                  break;
+              }
             }
 
             // always remove token if we match our file
