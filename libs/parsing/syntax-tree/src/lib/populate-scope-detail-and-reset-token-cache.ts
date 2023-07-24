8d7cd707844fc0e5c7f0e4350ca5600a49cabc0c
Zachary Norman <zachary.norman@nv5.com>
2023-07-24 10:43:39 -0600

    Add 2 for 1 routines to reduce tree loops

---
+++ libs/parsing/syntax-tree/src/lib/populate-scope-detail-and-reset-token-cache.ts
@@ -0,0 +1,35 @@
+import { IParsed } from './build-tree.interface';
+import { TreeRecurser } from './recursion-and-callbacks/tree-recurser';
+import { ResetTokenCache } from './reset-token-cache';
+
+/**
+ * Populates scope detail information giving us a circular tree with
+ * our parent tokens and the tokens that are used for accessing which
+ * come before and are used for types.
+ *
+ * Also resets the token cache in the same tree iterator to reduce the number
+ * of times we need to process a tree
+ *
+ * Use `RemoveScopeDetail` to get rid of this information
+ */
+export function PopulateScopeDetailAndResetTokenCache(parsed: IParsed): void {
+  switch (true) {
+    case !parsed.hasDetail:
+      TreeRecurser(parsed, {
+        onBasicToken: (token, current) => {
+          token.scopeTokens = current.scopeTokens.slice();
+          token.accessTokens = current.accessTokens.slice();
+          token.cache = {};
+        },
+        onBranchToken: (token, current) => {
+          token.scopeTokens = current.scopeTokens.slice();
+          token.accessTokens = current.accessTokens.slice();
+          token.cache = {};
+        },
+      });
+      break;
+    default:
+      ResetTokenCache(parsed);
+      break;
+  }
+}
