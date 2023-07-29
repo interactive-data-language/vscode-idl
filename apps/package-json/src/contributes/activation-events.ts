409e030ac649a546e9101083de091fca7d9907c0
Zachary Norman <zachary.norman@nv5.com>
2023-07-29 13:48:22 -0600

    Update activation events

--- apps/package-json/src/contributes/activation-events.ts
+++ apps/package-json/src/contributes/activation-events.ts
@@ -1,5 +1,6 @@
 import {
   IDL_JSON_GLOB_PATTERN,
+  NOTEBOOK_GLOB_PATTERN,
   PRO_CODE_GLOB_PATTERN,
   TASK_FILE_GLOB_PATTERN,
 } from '@idl/shared';
@@ -9,9 +10,13 @@ import { IPackageJSON, IPackageNLS } from '../package.interface';
 export const BASE_ACTIVATION_EVENTS: string[] = [
   // not needed per https://code.visualstudio.com/updates/v1_74#_extension-authoring
   // `onLanguage:${IDL_LANGUAGE_NAME}`,
+  // `onLanguage:${IDL_LANGUAGE_NAME}`,
+  // `onLanguage:${LOG_LANGUAGE_NAME}`,
+  // `onLanguage:${IDL_NOTEBOOK_NAME}`,
   `workspaceContains:${PRO_CODE_GLOB_PATTERN}`,
   `workspaceContains:${IDL_JSON_GLOB_PATTERN}`,
   `workspaceContains:${TASK_FILE_GLOB_PATTERN}`,
+  `workspaceContains:${NOTEBOOK_GLOB_PATTERN}`,
 ];
 
 /**
