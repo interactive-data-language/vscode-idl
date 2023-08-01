3906dff77d3db0fef8f9d3c65cfcd01214d6e35b
Zachary Norman <zachary.norman@nv5.com>
2023-07-31 18:36:10 -0600

    Add test for LS startup, open, edit, close problem syncing

--- apps/client-e2e/src/tests/interactions/_interactions-runner.ts
+++ apps/client-e2e/src/tests/interactions/_interactions-runner.ts
@@ -2,6 +2,7 @@ import { Logger } from '@idl/logger';
 
 import { Runner } from '../runner.class';
 import { AddDocs } from './add-docs';
+import { IndexIDLFolderRightAndOpenEditClose } from './index-idl-folder-right-and-open-edit-close';
 
 /*
  * Logger to be used for tests related to debugging
@@ -18,9 +19,12 @@ export const INTERACTIONS_TEST_LOGGER = new Logger(
  */
 export const INTERACTIONS_RUNNER = new Runner(INTERACTIONS_TEST_LOGGER);
 
-// verify add-docs works
 INTERACTIONS_RUNNER.addTest({
   name: 'Add docs, update test, add docs again',
   fn: AddDocs,
-  critical: false,
+});
+
+INTERACTIONS_RUNNER.addTest({
+  name: 'Index IDL folder right on startup and properly sync problems on open, edit, close',
+  fn: IndexIDLFolderRightAndOpenEditClose,
 });
