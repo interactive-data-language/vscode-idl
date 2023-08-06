a229f7bb8d2c49dd8272400012e8ec5f1e288091
Zachary Norman <zachary.norman@nv5.com>
2023-08-06 11:56:34 -0600

    Fix outputs

--- libs/vscode/notebooks/src/lib/controller/idl-notebook-controller.class.ts
+++ libs/vscode/notebooks/src/lib/controller/idl-notebook-controller.class.ts
@@ -239,7 +239,7 @@ export class IDLNotebookController {
                 Buffer.from(
                   this._currentCell.output.replace(REGEX_NEW_LINE, '\n')
                 ),
-                'idl/test-mime'
+                'text/html'
               ),
             ])
           );
