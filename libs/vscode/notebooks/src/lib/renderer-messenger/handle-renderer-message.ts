6ebe3150fcc6782023b4b8f2b42185ee15d40e30
Zachary Norman <zachary.norman@nv5.com>
2023-08-07 14:08:23 -0600

    Allow saving image from renderer

--- libs/vscode/notebooks/src/lib/renderer-messenger/handle-renderer-message.ts
+++ libs/vscode/notebooks/src/lib/renderer-messenger/handle-renderer-message.ts
@@ -6,18 +6,36 @@ import {
 import { IDL_TRANSLATION } from '@idl/translation';
 import { IDL_LOGGER } from '@idl/vscode/client';
 
+import { SaveNotebookImage } from './save-notebook-image';
+
 /**
  * Handle messages from our renderer
  */
 export async function HandleRendererMessage(
   message: IDLNotebookRendererMessage<IDLNotebookRendererMessageType>
 ) {
+  IDL_LOGGER.log({
+    log: IDL_NOTEBOOK_LOG,
+    type: 'debug',
+    content: [`Received message type "${message.type}" from notebook renderer`],
+  });
+
   try {
-    IDL_LOGGER.log({
-      log: IDL_NOTEBOOK_LOG,
-      type: 'info',
-      content: [`received message type "${message.type}" from renderer`],
-    });
+    /**
+     * Handle messages
+     */
+    switch (message.type) {
+      /**
+       * Save PNG
+       */
+      case 'save-image':
+        await SaveNotebookImage(message.payload);
+        break;
+
+      default:
+        // do nothing
+        break;
+    }
   } catch (err) {
     IDL_LOGGER.log({
       log: IDL_NOTEBOOK_LOG,
---
+++ libs/vscode/notebooks/src/lib/renderer-messenger/save-notebook-image.ts
@@ -0,0 +1,31 @@
+import {
+  IDLNotebookEmbeddedItem,
+  IDLNotebookEncodedPNG,
+} from '@idl/notebooks/types';
+import { CleanPath } from '@idl/shared';
+import { writeFileSync } from 'fs';
+import * as vscode from 'vscode';
+
+/**
+ * Save image from a notebook
+ */
+export async function SaveNotebookImage(
+  payload: IDLNotebookEmbeddedItem<IDLNotebookEncodedPNG>
+) {
+  const res = await vscode.window.showSaveDialog({
+    filters: {
+      Images: ['png'],
+    },
+  });
+
+  // make sure we found something
+  if (res === undefined) {
+    return;
+  }
+
+  // write files to disk
+  writeFileSync(
+    CleanPath(res.fsPath),
+    Buffer.from(payload.item.data, 'base64')
+  );
+}
