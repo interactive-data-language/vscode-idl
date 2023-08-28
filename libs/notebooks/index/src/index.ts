fc7abba954e93926f5925ca35bfda546c0529fac
Zachary Norman <zachary.norman@nv5.com>
2023-08-27 21:49:57 -0600

    New lib for notebook helper, first pass at NB to PRO code

---
+++ libs/notebooks/index/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/notebooks-index';
---
+++ libs/notebooks/index/src/lib/notebook-to-pro-code.ts
@@ -0,0 +1,133 @@
+import { Assembler } from '@idl/assembler';
+import { FormatterType, IAssemblerOptions } from '@idl/assembling/config';
+import { IDLNotebookDocument } from '@idl/notebooks/shared';
+import { IDLIndex } from '@idl/parsing/index';
+import { SyntaxTree, TreeToken } from '@idl/parsing/syntax-tree';
+import { MainLevelToken, TOKEN_NAMES } from '@idl/parsing/tokenizer';
+import { writeFileSync } from 'fs';
+
+/**
+ * Converts a notebook to PRO code on disk
+ */
+export async function NotebookToProCode(
+  index: IDLIndex,
+  file: string,
+  notebook: IDLNotebookDocument,
+  outFile: string,
+  formatting: IAssemblerOptions<FormatterType>
+) {
+  /**
+   * Index our file
+   */
+  const indexed = await index.indexIDLNotebook(file, notebook);
+
+  /**
+   * Get the values for our parsed notebooks
+   */
+  const cells = Object.values(indexed);
+
+  /**
+   * Routine code (non-main)
+   */
+  let routines: string[] = [];
+
+  /**
+   * Main level program code
+   */
+  let main: string[] = [];
+
+  // track our strings
+  for (let i = 0; i < cells.length; i++) {
+    /** Get parsed cell */
+    const parsed = cells[i];
+
+    // save a reference to the tree
+    const tree = parsed.tree;
+
+    // skip if nothing
+    if (parsed.tree.length === 0) {
+      continue;
+    }
+
+    /** Non-main tokens */
+    let nonMainTokens: SyntaxTree = [];
+
+    /** Main level tokens */
+    let mainTokens: SyntaxTree = [];
+
+    // see how we need to split
+    if (tree[tree.length - 1].name === TOKEN_NAMES.MAIN_LEVEL) {
+      // check if we have something besides main
+      if (tree.length > 1) {
+        nonMainTokens = tree.slice(0, tree.length - 2);
+      }
+
+      // save main tokens
+      mainTokens = (tree[tree.length - 1] as TreeToken<MainLevelToken>).kids;
+    } else {
+      nonMainTokens = tree;
+    }
+
+    // check for non-main
+    if (nonMainTokens.length > 0) {
+      // get the syntax tree
+      parsed.tree = nonMainTokens;
+
+      // format the main level program
+      const nonMain = Assembler(parsed, formatting);
+
+      // TODO: figure out what to do if syntax error
+      if (nonMain === undefined) {
+        return;
+      }
+
+      // add extra line
+      if (routines.length > 0) {
+        routines.push('');
+      }
+
+      // combine
+      routines = routines.concat(nonMain.split(/\r?\n/g));
+    }
+
+    // check for main
+    if (mainTokens.length > 0) {
+      // format the main level program
+      const formattedMain = Assembler(parsed, formatting);
+
+      // TODO: figure out what to do if syntax error
+      if (formattedMain === undefined) {
+        return;
+      }
+
+      // add extra line
+      if (main.length > 0) {
+        main.push('');
+      }
+
+      // combine
+      main = main.concat(formattedMain.split(/\r?\n/g));
+    }
+  }
+
+  /**
+   * Strings we save to disk
+   */
+  let strings: string[] = [];
+
+  // combine routine strings
+  if (routines.length > 0) {
+    strings = strings.concat(routines);
+  }
+
+  // combine main strings
+  if (main.length > 0) {
+    if (strings.length > 0) {
+      strings.push('');
+    }
+    strings = strings.concat(main);
+  }
+
+  // write file to disk
+  writeFileSync(outFile, formatting.eol === 'lf' ? '\n' : '\r\n');
+}
