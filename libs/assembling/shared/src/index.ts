e3c1fac9a730ffe80924750c3219e38fe9530612
Zachary Norman <zachary.norman@nv5.com>
2023-11-06 22:34:14 -0700

    Change filename to match function

--- libs/assembling/shared/src/index.ts
+++ libs/assembling/shared/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/add-code-to-syntax-tree';
-export * from './lib/case-transforms';
+export * from './lib/adjust-case';
 export * from './lib/conditional-line-number-increment';
 export * from './lib/increment-line-numbers';
