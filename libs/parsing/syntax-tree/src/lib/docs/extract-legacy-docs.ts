2a3c10e17e5301a18f3f314adce570570f57964b
Zachary Norman <zachary.norman@nv5.com>
2023-11-21 11:00:10 -0700

    WIP: Attempt to resolve regex performance issue

--- libs/parsing/syntax-tree/src/lib/docs/extract-legacy-docs.ts
+++ libs/parsing/syntax-tree/src/lib/docs/extract-legacy-docs.ts
@@ -1,7 +1,8 @@
 import { CommentToken, GetMatchesArray } from '@idl/parsing/tokenizer';
+import copy from 'fast-copy';
 import { deepEqual } from 'fast-equals';
 
-import { IBasicBranch } from '../branches.interface';
+import { IBasicBranch, TreeToken } from '../branches.interface';
 import { IDL_DOCS_HEADERS } from './docs.interface';
 import {
   HEADER_TAG_LEGACY,
@@ -15,6 +16,42 @@ import {
   REMOVE_COMMENT_REGEX,
 } from './extract-docs.interface';
 import { IDL_HEADER_MAP } from './header-map.interface';
+
+/**
+ * Updates all last found with different items
+ */
+function AddDocsToLastFound(
+  lastFound: IHeaderDocs[],
+  docs: string,
+  comment: TreeToken<CommentToken>
+) {
+  for (let i = 0; i < lastFound.length; i++) {
+    lastFound[i].docs.push(docs);
+    lastFound[i].comments.push(comment);
+  }
+}
+
+/**
+ * Combines last found content together after splitting
+ */
+function MergeLastFound(lastFound: IHeaderDocs[]) {
+  if (lastFound.length < 2) {
+    return;
+  }
+
+  /**
+   * Save what we add to
+   */
+  const first = lastFound[0];
+
+  /**
+   * Process all other entries
+   */
+  for (let i = 1; i < lastFound.length; i++) {
+    first.docs = first.docs.concat(lastFound[i].docs);
+    first.comments = first.comments.concat(lastFound[i].comments);
+  }
+}
 
 /**
  * When we don't have anything extra within our docs, we attempt
@@ -69,7 +106,7 @@ export function ExtractLegacyDocs(
   };
 
   /** Track the last found block which we will save text to if a new block is not found */
-  let lastFound: IHeaderDocs = blocks[IDL_DOCS_HEADERS.DEFAULT];
+  let lastFound: IHeaderDocs[] = [blocks[IDL_DOCS_HEADERS.DEFAULT]];
 
   /** Match for testing string */
   let match: RegExpExecArray;
@@ -121,32 +158,37 @@ export function ExtractLegacyDocs(
        * Check if we have a new key or an existing one
        */
       if (key in blocks) {
-        lastFound = blocks[key];
+        lastFound = [blocks[key]];
       } else {
         /**
          * Make new key
          */
 
+        // merge docs together
+        MergeLastFound(lastFound);
+
         // make new last found
-        lastFound = {
-          pos: [
-            comments[i].pos[0],
-            match.index + 1 + startLoc,
-            match[0].length,
-          ],
-          matches: GetMatchesArray(match),
-          end: [
-            comments[i].pos[0],
-            match.index + 1 + startLoc,
-            match[0].length,
-          ],
-          docs: [],
-          comments: [],
-          type: 'idldoc-legacy',
-        };
+        lastFound = [
+          {
+            pos: [
+              comments[i].pos[0],
+              match.index + 1 + startLoc,
+              match[0].length,
+            ],
+            matches: GetMatchesArray(match),
+            end: [
+              comments[i].pos[0],
+              match.index + 1 + startLoc,
+              match[0].length,
+            ],
+            docs: [],
+            comments: [],
+            type: 'idldoc-legacy',
+          },
+        ];
 
         // save our new block
-        blocks[key] = lastFound;
+        blocks[key] = lastFound[0];
       }
 
       // get the text afterwards if we have any
@@ -161,28 +203,46 @@ export function ExtractLegacyDocs(
         ) {
           // get parameter name
           const nameMatch = LEGACY_PARAMETER_NAME_SPLIT.exec(after);
+
+          /** Get the name */
           const name = nameMatch !== null ? nameMatch[0] : '';
 
-          // put official RST docs
-          lastFound.docs.push(
-            `   ${name}: ${
-              LEGACY_PARAMETER_DIRECTION.test(after) ? 'in' : 'bidirectional'
-            }, required, any`
-          );
-          lastFound.comments.push(comments[i]);
+          /** Split on commas for multi-parameter docs */
+          const split = name.split(/,/gim);
 
-          // save description
-          lastFound.docs.push(
+          /** Copy the last lastFound item */
+          const copyBase = copy(lastFound[0]);
+
+          /** Add all names */
+          for (let zz = 0; zz < split.length; zz++) {
+            /** Figure out what we should add to */
+            const addTo = zz === 0 ? lastFound[0] : copy(copyBase);
+
+            // save line for docs
+            addTo.docs.push(
+              `   ${split[zz]}: ${
+                LEGACY_PARAMETER_DIRECTION.test(after) ? 'in' : 'bidirectional'
+              }, required, any`
+            );
+            addTo.comments.push(comments[i]);
+
+            // save so we track for each
+            if (zz > 0) {
+              lastFound.push(addTo);
+            }
+          }
+
+          AddDocsToLastFound(
+            lastFound,
             '     ' +
               after
                 .substring(name.length)
                 .replace(LEGACY_PARAMETER_DIRECTION, '')
-                .trim()
+                .trim(),
+            comments[i]
           );
-          lastFound.comments.push(comments[i]);
         } else {
-          lastFound.docs.push(after);
-          lastFound.comments.push(comments[i]);
+          AddDocsToLastFound(lastFound, after, comments[i]);
         }
 
         startSaving = true;
@@ -199,16 +259,22 @@ export function ExtractLegacyDocs(
 
     // save line
     if (startSaving) {
-      lastFound.docs.push(
+      AddDocsToLastFound(
+        lastFound,
         key === IDL_DOCS_HEADERS.ARGS || key === IDL_DOCS_HEADERS.KEYWORDS
           ? `    ${line}`
-          : line
+          : line,
+        comments[i]
       );
-      lastFound.comments.push(comments[i]);
     }
 
     // update the last line we found text on
     // add a bonus "+1" because we should have our comment on the first line stripped out
-    lastFound.end = [comments[i].pos[0], 0, line.length + startLoc];
+    for (let zz = 0; zz < lastFound.length; zz++) {
+      lastFound[zz].end = [comments[i].pos[0], 0, line.length + startLoc];
+    }
   }
+
+  // merge any final lastFound items together
+  MergeLastFound(lastFound);
 }
