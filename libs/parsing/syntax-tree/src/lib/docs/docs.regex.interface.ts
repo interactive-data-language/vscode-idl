4f7354458deeebc2fcf0093eeef0a69039d6cff1
Zachary Norman <zachary.norman@nv5.com>
2023-11-29 21:29:38 -0700

    Update tests and regex to filter links

--- libs/parsing/syntax-tree/src/lib/docs/docs.regex.interface.ts
+++ libs/parsing/syntax-tree/src/lib/docs/docs.regex.interface.ts
@@ -6,7 +6,8 @@ export const HEADER_TAG = /^\s*(?<!:):(?!:)([^:]+)(?<!:):(?!:)/i;
 /**
  * Regex to detect arguments or keywords
  */
-export const ARG_KW_PROPERTY_TAG = /^\s*([^:\s]*)\s*(?<!:):(?!:)/i;
+export const ARG_KW_PROPERTY_TAG =
+  /^\s*([^:\s]*)\s*(?<!:|https?|ftp|s3|file):(?!:)/i;
 // export const ARG_KW_PROPERTY_TAG = /^\s*([^:\s]*)\s*(?<!:):(?!:)/i;
 
 /**
