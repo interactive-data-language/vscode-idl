9b8a69f6b83437adbfa68135c5a18a08f96b3b90
Zachary Norman <zachary.norman@nv5.com>
2023-12-07 11:32:11 -0700

    Re-work TODO detection

--- libs/parsing/syntax-validators/src/lib/validators/comment-to-do.ts
+++ libs/parsing/syntax-validators/src/lib/validators/comment-to-do.ts
@@ -6,17 +6,27 @@ import {
 import { TOKEN_NAMES } from '@idl/parsing/tokenizer';
 
 /**
+ * Regex to extract TODO statements
+ */
+const TODO_REGEX = /^TODO:/im;
+
+/**
  * Extract TODO statements from comments
  */
 IDL_SYNTAX_TREE_VALIDATOR.onBasicToken(TOKEN_NAMES.COMMENT, (token, parsed) => {
+  /**
+   * Remove leading comment
+   */
+  const sub = token.match[0].substring(1).trim();
+
   // make sure we have a TODO which means three capture groups
   // see comments.spec.ts for some examples
-  if (token.match.length === 3) {
+  if (TODO_REGEX.test(sub)) {
     token.parseProblems.push(IDL_PROBLEM_CODES.TODO);
     parsed.parseProblems.push(
       SyntaxProblemWithoutTranslation(
         IDL_PROBLEM_CODES.TODO,
-        `TODO: ${token.match[2].trim()}`,
+        `TODO: ${sub.substring(5).trim()}`,
         token.pos,
         token.pos
       )
