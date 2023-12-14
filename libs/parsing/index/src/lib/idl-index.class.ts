a56e8cd5101e1e41fc4d4b55f0d56faa6092ef10
Zachary Norman <zachary.norman@nv5.com>
2023-12-14 12:33:12 -0700

    Fix issue with notebook cell go-to-def being broken with new data pipes

--- libs/parsing/index/src/lib/idl-index.class.ts
+++ libs/parsing/index/src/lib/idl-index.class.ts
@@ -1120,6 +1120,9 @@ export class IDLIndex {
       byCell[cellFSPath] = Parser(cell.text, token, {
         isNotebook: true,
       });
+
+      // track global tokens
+      this.globalIndex.trackGlobalTokens(byCell[cellFSPath].global, cellFSPath);
     }
 
     /**
