bc65f9e474fed84d8ddf920d0e68df27227aeb30
Zachary Norman <zachary.norman@nv5.com>
2024-01-02 11:03:44 -0700

    Add missing import

--- libs/vscode/server/src/lib/events/is-initialized.ts
+++ libs/vscode/server/src/lib/events/is-initialized.ts
@@ -14,6 +14,7 @@ import {
   USAGE_METRIC_LOOKUP,
 } from '@idl/usage-metrics';
 import { LANGUAGE_SERVER_MESSAGE_LOOKUP } from '@idl/vscode/events/messages';
+import { DEFAULT_IDL_EXTENSION_CONFIG } from '@idl/vscode/extension-config';
 import { arch, cpus, platform } from 'os';
 
 import { CacheValidFSPath } from '../helpers/cache-valid';
