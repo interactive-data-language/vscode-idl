ab768f6ba6b6d33685a55ba5607153811831e7bd
Zachary Norman <zachary.norman@nv5.com>
2024-01-31 10:46:20 -0700

    Replace markdown with docs URLs

--- libs/vscode/server/src/lib/helpers/resolve-extension-docs-url.ts
+++ libs/vscode/server/src/lib/helpers/resolve-extension-docs-url.ts
@@ -1,3 +1,5 @@
+import { EXTENSION_DOCS_URL } from '@idl/shared';
+
 import { IDL_CLIENT_CONFIG } from './track-workspace-config';
 
 /**
@@ -6,7 +8,7 @@ import { IDL_CLIENT_CONFIG } from './track-workspace-config';
 export function ResolveExtensionDocsURL(baseUrl: string) {
   return `${
     IDL_CLIENT_CONFIG.documentation.useOnline
-      ? 'https://interactive-data-language.github.io/vscode-idl'
+      ? EXTENSION_DOCS_URL
       : `http://localhost:${IDL_CLIENT_CONFIG.documentation.localPort}`
   }${baseUrl}`;
 }
