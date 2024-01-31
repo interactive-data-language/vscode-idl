aeb27e968fd6b00911970694ec5f82f325fdedef
Zachary Norman <zachary.norman@nv5.com>
2024-01-31 10:24:55 -0700

    Update docs with problem codes and placeholders

--- extension/docs/.vitepress/sidebars/problem-codes.sidebar.ts
+++ extension/docs/.vitepress/sidebars/problem-codes.sidebar.ts
@@ -1,7 +1,9 @@
 import { DefaultTheme } from 'vitepress';
 
 /**
- * Sidebar entries for the FAQ
+ * Sidebar entries for all problem codes
+ * 
+ * Generated automatically when we build our package.json file
  */
 export const PROBLEM_CODES_SIDEBAR: DefaultTheme.NavItemWithLink[] = [
   {
@@ -51,6 +53,10 @@ export const PROBLEM_CODES_SIDEBAR: DefaultTheme.NavItemWithLink[] = [
   {
     text: '"illegal-include"',
     link: '/problem-codes/codes/11.md',
+  },
+  {
+    text: '"reserved-var"',
+    link: '/problem-codes/codes/12.md',
   },
   {
     text: '"illegal-ternary"',
@@ -113,6 +119,10 @@ export const PROBLEM_CODES_SIDEBAR: DefaultTheme.NavItemWithLink[] = [
     link: '/problem-codes/codes/27.md',
   },
   {
+    text: '"duplicate-sys-var"',
+    link: '/problem-codes/codes/28.md',
+  },
+  {
     text: '"reserved-pro"',
     link: '/problem-codes/codes/29.md',
   },
@@ -149,6 +159,118 @@ export const PROBLEM_CODES_SIDEBAR: DefaultTheme.NavItemWithLink[] = [
     link: '/problem-codes/codes/37.md',
   },
   {
+    text: '"no-comp-opt"',
+    link: '/problem-codes/codes/38.md',
+  },
+  {
+    text: '"no-idl2"',
+    link: '/problem-codes/codes/39.md',
+  },
+  {
+    text: '"illegal-comp-opt"',
+    link: '/problem-codes/codes/40.md',
+  },
+  {
+    text: '"empty-comp-opt"',
+    link: '/problem-codes/codes/41.md',
+  },
+  {
+    text: '"use-idl2"',
+    link: '/problem-codes/codes/42.md',
+  },
+  {
+    text: '"expected-comma"',
+    link: '/problem-codes/codes/43.md',
+  },
+  {
+    text: '"unexpected-comma"',
+    link: '/problem-codes/codes/44.md',
+  },
+  {
+    text: '"multiple-comp-opt"',
+    link: '/problem-codes/codes/45.md',
+  },
+  {
+    text: '"unclosed-quote"',
+    link: '/problem-codes/codes/46.md',
+  },
+  {
+    text: '"args-first"',
+    link: '/problem-codes/codes/47.md',
+  },
+  {
+    text: '"docs-missing-arg"',
+    link: '/problem-codes/codes/48.md',
+  },
+  {
+    text: '"no-args-to-doc"',
+    link: '/problem-codes/codes/49.md',
+  },
+  {
+    text: '"docs-missing-kw"',
+    link: '/problem-codes/codes/50.md',
+  },
+  {
+    text: '"no-kws-to-doc"',
+    link: '/problem-codes/codes/51.md',
+  },
+  {
+    text: '"docs-missing-return"',
+    link: '/problem-codes/codes/52.md',
+  },
+  {
+    text: '"docs-invalid-in-out"',
+    link: '/problem-codes/codes/53.md',
+  },
+  {
+    text: '"docs-invalid-require"',
+    link: '/problem-codes/codes/54.md',
+  },
+  {
+    text: '"docs-invalid-type"',
+    link: '/problem-codes/codes/55.md',
+  },
+  {
+    text: '"docs-invalid-private"',
+    link: '/problem-codes/codes/56.md',
+  },
+  {
+    text: '"docs-too-few-params"',
+    link: '/problem-codes/codes/57.md',
+  },
+  {
+    text: '"docs-too-many-params"',
+    link: '/problem-codes/codes/58.md',
+  },
+  {
+    text: '"docs-left-align"',
+    link: '/problem-codes/codes/59.md',
+  },
+  {
+    text: '"docs-return-has-no-type"',
+    link: '/problem-codes/codes/60.md',
+  },
+  {
+    text: '"docs-return-invalid"',
+    link: '/problem-codes/codes/61.md',
+  },
+  {
+    text: '"docs-return-not-needed"',
+    link: '/problem-codes/codes/62.md',
+  },
+  {
+    text: '"docs-not-real-param"',
+    link: '/problem-codes/codes/63.md',
+  },
+  {
+    text: '"docs-param-missing"',
+    link: '/problem-codes/codes/64.md',
+  },
+  {
+    text: '"string-literal-too-many-args"',
+    link: '/problem-codes/codes/65.md',
+  },
+  {
     text: '"bad-continue"',
     link: '/problem-codes/codes/66.md',
   },
@@ -159,6 +281,150 @@ export const PROBLEM_CODES_SIDEBAR: DefaultTheme.NavItemWithLink[] = [
   {
     text: '"expected-statement"',
     link: '/problem-codes/codes/68.md',
+  },
+  {
+    text: '"unfinished-dot"',
+    link: '/problem-codes/codes/69.md',
+  },
+  {
+    text: '"illegal-hex-escape"',
+    link: '/problem-codes/codes/70.md',
+  },
+  {
+    text: '"unknown-template-escape"',
+    link: '/problem-codes/codes/71.md',
+  },
+  {
+    text: '"duplicate-arg-kw-var-def"',
+    link: '/problem-codes/codes/72.md',
+  },
+  {
+    text: '"duplicate-kw-def"',
+    link: '/problem-codes/codes/73.md',
+  },
+  {
+    text: '"duplicate-property"',
+    link: '/problem-codes/codes/74.md',
+  },
+  {
+    text: '"duplicate-kw-usage"',
+    link: '/problem-codes/codes/75.md',
+  },
+  {
+    text: '"init-method-pro"',
+    link: '/problem-codes/codes/76.md',
+  },
+  {
+    text: '"unknown-structure"',
+    link: '/problem-codes/codes/77.md',
+  },
+  {
+    text: '"illegal-chain"',
+    link: '/problem-codes/codes/78.md',
+  },
+  {
+    text: '"docs-missing-struct"',
+    link: '/problem-codes/codes/79.md',
+  },
+  {
+    text: '"docs-missing-prop"',
+    link: '/problem-codes/codes/80.md',
+  },
+  {
+    text: '"class-no-params"',
+    link: '/problem-codes/codes/81.md',
+  },
+  {
+    text: '"docs-prop-too-few-params"',
+    link: '/problem-codes/codes/82.md',
+  },
+  {
+    text: '"docs-prop-too-many-params"',
+    link: '/problem-codes/codes/83.md',
+  },
+  {
+    text: '"illegal-subscript"',
+    link: '/problem-codes/codes/84.md',
+  },
+  {
+    text: '"illegal-struct-op"',
+    link: '/problem-codes/codes/85.md',
+  },
+  {
+    text: '"illegal-list-op"',
+    link: '/problem-codes/codes/86.md',
+  },
+  {
+    text: '"illegal-hash-op"',
+    link: '/problem-codes/codes/87.md',
+  },
+  {
+    text: '"illegal-ordered-hash-op"',
+    link: '/problem-codes/codes/88.md',
+  },
+  {
+    text: '"illegal-dictionary-op"',
+    link: '/problem-codes/codes/89.md',
+  },
+  {
+    text: '"potential-type-incompatibility"',
+    link: '/problem-codes/codes/90.md',
+  },
+  {
+    text: '"illegal-index-type"',
+    link: '/problem-codes/codes/91.md',
+  },
+  {
+    text: '"potential-arr-type-incompatibility"',
+    link: '/problem-codes/codes/92.md',
+  },
+  {
+    text: '"ptr-nothing-to-de-ref"',
+    link: '/problem-codes/codes/93.md',
+  },
+  {
+    text: '"ptr-de-ref-illegal"',
+    link: '/problem-codes/codes/94.md',
+  },
+  {
+    text: '"indexing-error"',
+    link: '/problem-codes/codes/95.md',
+  },
+  {
+    text: '"ptr-de-ref-ambiguity"',
+    link: '/problem-codes/codes/96.md',
+  },
+  {
+    text: '"unknown-kw"',
+    link: '/problem-codes/codes/97.md',
+  },
+  {
+    text: '"incomplete-ternary"',
+    link: '/problem-codes/codes/98.md',
+  },
+  {
+    text: '"undefined-var"',
+    link: '/problem-codes/codes/99.md',
+  },
+  {
+    text: '"potential-undefined-var"',
+    link: '/problem-codes/codes/100.md',
+  },
+  {
+    text: '"var-use-before-def"',
+    link: '/problem-codes/codes/101.md',
+  },
+  {
+    text: '"potential-var-use-before-def"',
+    link: '/problem-codes/codes/102.md',
+  },
+  {
+    text: '"ambiguous-keyword-abbreviation"',
+    link: '/problem-codes/codes/103.md',
+  },
+  {
+    text: '"unused-var"',
+    link: '/problem-codes/codes/104.md',
   },
   {
     text: '"illegal-var-index"',
