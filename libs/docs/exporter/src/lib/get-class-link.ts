cfe2187d0fdb1f758319aa8532eee3e602c2a3a5
Zachary Norman <zachary.norman@nv5.com>
2024-02-03 16:47:56 -0700

    Get sidebar for class sorted out

---
+++ libs/docs/exporter/src/lib/get-class-link.ts
@@ -0,0 +1,9 @@
+import { DOCS_BASE } from './docs-exporter.interface';
+import { DOCS_PATHS } from './folder-map.interface';
+
+/**
+ * Gets the link for a class
+ */
+export function GetClassLink(name: string) {
+  return `${DOCS_BASE}/${DOCS_PATHS.CLASS}/${name}.md`;
+}
