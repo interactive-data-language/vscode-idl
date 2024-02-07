11f39bb493fe436c2df3fc173818ce1d1c18336a
Zachary Norman <zachary.norman@nv5.com>
2024-02-06 22:11:18 -0700

    Apply disabled problems to what we detect

--- libs/parsing/index/src/lib/helpers/get-syntax-problems.ts
+++ libs/parsing/index/src/lib/helpers/get-syntax-problems.ts
@@ -1,10 +1,14 @@
+import { ApplyDisabledProblems } from '@idl/parser';
 import { IParsed } from '@idl/parsing/syntax-tree';
 
 /**
  * From a parsed file, returns all syntax problems.
  *
  * Simple helper because we track these problems in more than one place
+ * and this makes sure problems are up-to-date with what can be reported
+ * or not
  */
 export function GetSyntaxProblems(parsed: IParsed) {
+  ApplyDisabledProblems(parsed);
   return parsed.parseProblems.concat(parsed.postProcessProblems);
 }
