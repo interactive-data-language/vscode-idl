816e8ccbc5bab672a9b2d84bba68c460b20ad3c6
Zachary Norman <zachary.norman@nv5.com>
2024-02-06 20:49:37 -0700

    Make lib for syntax tree types

---
+++ libs/types/syntax-tree/src/lib/types/syntax-tree.spec.ts
@@ -0,0 +1,7 @@
+import { typesSyntaxTree } from './types/syntax-tree';
+
+describe('typesSyntaxTree', () => {
+  it('should work', () => {
+    expect(typesSyntaxTree()).toEqual('types/syntax-tree');
+  });
+});
