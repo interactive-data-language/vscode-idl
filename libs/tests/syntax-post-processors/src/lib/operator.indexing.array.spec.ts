013267c5992eef12f74f2035ac0eb27198ecd476
Zachary Norman <zachary.norman@nv5.com>
2024-02-07 15:34:21 -0700

    Update test imports for name changes

--- libs/tests/syntax-post-processors/src/lib/operator.indexing.array.spec.ts
+++ libs/tests/syntax-post-processors/src/lib/operator.indexing.array.spec.ts
@@ -1,7 +1,7 @@
 import { CancellationToken } from '@idl/cancellation-tokens';
 import { Parser } from '@idl/parser';
-import { SyntaxProblems } from '@idl/types/problem-codes';
 import { SyntaxTree } from '@idl/parsing/syntax-tree';
+import { SyntaxProblems } from '@idl/types/problem-codes';
 
 describe(`[auto generated] Correctly identify array indexing`, () => {
   it(`[auto generated] all parts of ternary operators`, () => {
