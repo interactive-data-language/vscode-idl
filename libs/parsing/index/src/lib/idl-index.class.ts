3a62b9b1e0f06f082cb90ef8ec7b88b06cc0b3ca
Zachary Norman <zachary.norman@nv5.com>
2024-02-09 12:09:29 -0700

    Track known files to fix bug

--- libs/parsing/index/src/lib/idl-index.class.ts
+++ libs/parsing/index/src/lib/idl-index.class.ts
@@ -2217,11 +2217,17 @@ export class IDLIndex {
      */
     const files = await this.findFiles(folder);
 
+    // add all files to known
+    for (let i = 0; i < files.length; i++) {
+      this.knownFiles[files[i]] = undefined;
+    }
+
     /**
      * Bucket them into separate groups
      */
     const buckets = this.bucketFiles(files);
 
+    // create a cancellation token
     const token = new CancellationToken();
 
     // save discovery time
@@ -2272,9 +2278,16 @@ export class IDLIndex {
       this.removeWorkspaceFiles([file]);
       return;
     }
+
+    // mark as known file
+    this.knownFiles[file] = undefined;
+
+    // get code if we dont have it
     if (code === undefined) {
       code = this.getFileStrings(file);
     }
+
+    // index appropriately
     switch (true) {
       case this.isConfigFile(file):
         await this.indexConfigFile(file, code);
@@ -2304,6 +2317,11 @@ export class IDLIndex {
     cb: (file: string) => Promise<string>,
     token: CancellationToken
   ) {
+    // add all files to known
+    for (let i = 0; i < files.length; i++) {
+      this.knownFiles[files[i]] = undefined;
+    }
+
     /**
      * Bucket files
      */
