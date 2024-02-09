b430a50c0abfa3d891d0d5e40265da6b9fc39349
Zachary Norman <zachary.norman@nv5.com>
2024-02-09 12:09:51 -0700

    Simplify code to reset extension settings

--- apps/client-e2e/src/reset-settings-for-tests.ts
+++ apps/client-e2e/src/reset-settings-for-tests.ts
@@ -10,110 +10,80 @@ import {
 const SCOPE = true;
 
 /**
+ * Wrapper to correctly set config because of nonsense settings API
+ */
+async function ResetConfigToDefault(
+  config: IIDLWorkspaceConfig,
+  key: string,
+  obj: { [key: string]: any }
+) {
+  /** Get keys from the data we need to reset */
+  const keys = Object.keys(obj);
+
+  // reset all keys
+  for (let i = 0; i < keys.length; i++) {
+    const settingsKey = `${key}.${keys[i]}`;
+
+    // make sure exists - we merge with internal settings that are
+    // not always exposed
+    if (config.has(settingsKey)) {
+      await config.update(settingsKey, obj[keys[i]], SCOPE);
+    }
+  }
+}
+
+/**
  * Reset extension config for running tests to make sure it is always fresh and consistent
  */
 export async function ResetSettingsForTests(config: IIDLWorkspaceConfig) {
   /**
-   * Top-level config
+   * Top-level settings
    */
   await config.update(
     IDL_EXTENSION_CONFIG_KEYS.debugMode,
     DEFAULT_IDL_EXTENSION_CONFIG.debugMode,
     SCOPE
   );
-
-  /**
-   * IDL config
-   */
-  await config.update(
-    IDL_EXTENSION_CONFIG_KEYS.IDL,
-    DEFAULT_IDL_EXTENSION_CONFIG.IDL,
-    SCOPE
-  );
-
-  /**
-   * code config
-   */
-  await config.update(
-    IDL_EXTENSION_CONFIG_KEYS.code,
-    DEFAULT_IDL_EXTENSION_CONFIG.code,
-    SCOPE
-  );
-  await config.update(
-    IDL_EXTENSION_CONFIG_KEYS.codeFormatting,
-    DEFAULT_IDL_EXTENSION_CONFIG.code.formatting,
-    SCOPE
-  );
-
-  /**
-   * Documentation
-   */
-  await config.update(
-    IDL_EXTENSION_CONFIG_KEYS.documentation,
-    DEFAULT_IDL_EXTENSION_CONFIG.documentation,
-    SCOPE
-  );
-
-  /**
-   * Language server
-   */
-  await config.update(
-    IDL_EXTENSION_CONFIG_KEYS.languageServer,
-    DEFAULT_IDL_EXTENSION_CONFIG.languageServer,
-    SCOPE
-  );
-
-  /**
-   * Problem reporting
-   */
-  await config.update(
-    IDL_EXTENSION_CONFIG_KEYS.problems,
-    DEFAULT_IDL_EXTENSION_CONFIG.problems,
-    SCOPE
-  );
-
-  /**
-   * Notebooks
-   */
-  await config.update(
-    IDL_EXTENSION_CONFIG_KEYS.notebooks,
-    DEFAULT_IDL_EXTENSION_CONFIG.notebooks,
-    SCOPE
-  );
-
-  /**
-   * Questions
-   */
   await config.update(
     IDL_EXTENSION_CONFIG_KEYS.dontAsk,
     DEFAULT_IDL_EXTENSION_CONFIG.dontAsk,
     SCOPE
   );
+  if (config.has(IDL_EXTENSION_CONFIG_KEYS.dontShow)) {
+    await config.update(
+      IDL_EXTENSION_CONFIG_KEYS.dontShow,
+      DEFAULT_IDL_EXTENSION_CONFIG.dontShow,
+      SCOPE
+    );
+  }
 
   /**
-   * Things we show
+   * Batch reset everything else
    */
-  await config.update(
-    IDL_EXTENSION_CONFIG_KEYS.dontShow,
-    DEFAULT_IDL_EXTENSION_CONFIG.dontShow,
-    SCOPE
-  );
+  const toReset: [string, { [key: string]: any }][] = [
+    [IDL_EXTENSION_CONFIG_KEYS.IDL, DEFAULT_IDL_EXTENSION_CONFIG.IDL],
+    [IDL_EXTENSION_CONFIG_KEYS.code, DEFAULT_IDL_EXTENSION_CONFIG.code],
+    [
+      IDL_EXTENSION_CONFIG_KEYS.documentation,
+      DEFAULT_IDL_EXTENSION_CONFIG.documentation,
+    ],
+    [
+      IDL_EXTENSION_CONFIG_KEYS.languageServer,
+      DEFAULT_IDL_EXTENSION_CONFIG.languageServer,
+    ],
+    [IDL_EXTENSION_CONFIG_KEYS.problems, DEFAULT_IDL_EXTENSION_CONFIG.problems],
+    [
+      IDL_EXTENSION_CONFIG_KEYS.notebooks,
+      DEFAULT_IDL_EXTENSION_CONFIG.notebooks,
+    ],
+    [
+      IDL_EXTENSION_CONFIG_KEYS.developer,
+      DEFAULT_IDL_EXTENSION_CONFIG.developer,
+    ],
+  ];
 
-  /**
-   * Things we show
-   */
-  await config.update(
-    IDL_EXTENSION_CONFIG_KEYS.dontShow,
-    DEFAULT_IDL_EXTENSION_CONFIG.dontShow,
-    SCOPE
-  );
-
-  /**
-   * Developer
-   */
-  await config.update(
-    IDL_EXTENSION_CONFIG_KEYS.developer,
-    DEFAULT_IDL_EXTENSION_CONFIG.developer,
-    SCOPE
-  );
+  // reset the settings
+  for (let i = 0; i < toReset.length; i++) {
+    await ResetConfigToDefault(config, toReset[i][0], toReset[i][1]);
+  }
 }
