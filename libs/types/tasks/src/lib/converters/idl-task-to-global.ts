13f03df8d4d4e203fd1426f4cebe297d3cff7b1d
Zachary Norman <zachary.norman@nv5.com>
2024-02-14 09:28:26 -0700

    Change docs logic and fix bug with task naming

--- libs/types/tasks/src/lib/converters/idl-task-to-global.ts
+++ libs/types/tasks/src/lib/converters/idl-task-to-global.ts
@@ -31,7 +31,7 @@ export function IDLTaskToGlobal(
     name: useName,
     pos: [0, 0, 0],
     meta: {
-      display: name.toLowerCase(),
+      display: name,
       source: GLOBAL_TOKEN_SOURCE_LOOKUP.USER,
       docs: task.description,
       private: false,
