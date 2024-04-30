0c8fcb6aff2eeeb313acfeb1d86fe4f3459f6a35
Zachary Norman <zacharyanorman@gmail.com>
2024-04-29 23:00:57 -0600

    Update tests

--- libs/tests/syntax-validators/src/lib/code.16.double-token.exclusions1.spec.ts
+++ libs/tests/syntax-validators/src/lib/code.16.double-token.exclusions1.spec.ts
@@ -44,4 +44,50 @@ describe(`[auto generated] Allows these tokens next to each other`, () => {
       tokenized.parseProblems.concat(tokenized.postProcessProblems)
     ).toEqual(expected);
   });
+
+  it(`[auto generated] indexed properties`, async () => {
+    // create index
+    const index = new IDLIndex(
+      new LogManager({
+        alert: () => {
+          // do nothing
+        },
+      }),
+      0
+    );
+
+    // test code to extract tokens from
+    const code = [`a = b.(0).(0).(1)`];
+
+    // extract tokens
+    const tokenized = await index.getParsedProCode(
+      'not-real',
+      code,
+      new CancellationToken(),
+      { postProcess: true }
+    );
+
+    // define expected tokens
+    const expected: SyntaxProblems = [
+      {
+        code: 99,
+        info: 'Undefined variable "b"',
+        start: [0, 4, 1],
+        end: [0, 4, 1],
+        canReport: true,
+      },
+      {
+        code: 104,
+        info: 'Unused variable "a"',
+        start: [0, 0, 1],
+        end: [0, 0, 1],
+        canReport: true,
+      },
+    ];
+
+    // verify results
+    expect(
+      tokenized.parseProblems.concat(tokenized.postProcessProblems)
+    ).toEqual(expected);
+  });
 });
