c618f0ddcf798d939dff225161d6abdf5c60c0c9
Zachary Norman <zachary.norman@nv5.com>
2024-05-15 19:06:06 -0600

    Update semantic token generator tests, add new one for NB printing

--- libs/tests/semantic-tokens/src/lib/notebooks.spec.ts
+++ libs/tests/semantic-tokens/src/lib/notebooks.spec.ts
@@ -5,7 +5,7 @@ import { IDL_INDEX_OPTIONS, IDLIndex } from '@idl/parsing/index';
 IDL_INDEX_OPTIONS.IS_TEST = true;
 
 describe(`[auto generated] Extracts semantic tokens`, () => {
-  it(`[auto generated] for complex case with out-of order and same ref`, async () => {
+  it(`[auto generated] for notebook cells`, async () => {
     // create index
     const index = new IDLIndex(
       new LogManager({
@@ -17,29 +17,18 @@ describe(`[auto generated] Extracts semantic tokens`, () => {
     );
 
     // test code to extract tokens from
-    const code = [
-      `compile_opt idl2`,
-      `IDLgrVolume.AddToNotebookMap`,
-      `!null = ENVI.displayinnotebookmap + ENVI.api_version`,
-      ``,
-      `IDLgrVolume.AddToNotebookMap`,
-      `ENVI.displayInNotebookMap`,
-      `end`,
-    ];
+    const code = [`compile_opt idl2`, `arr = [1,2,3,4]`, `arr`, `end`];
 
     // extract tokens
     const tokenized = await index.getParsedProCode(
       'not-real',
       code,
       new CancellationToken(),
-      { postProcess: true }
+      { postProcess: true, isNotebook: true }
     );
 
     // define expected tokens
-    const expected = [
-      1, 0, 11, 0, 0, 1, 8, 4, 0, 0, 0, 28, 4, 0, 0, 2, 0, 11, 0, 0, 1, 0, 4, 0,
-      0,
-    ];
+    const expected = [2, 0, 3, 1, 1];
 
     // verify results
     expect(tokenized.semantic.built.data).toEqual(expected);
