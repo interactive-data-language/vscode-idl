4dff6e64c0999cc56dad4c2cc2557d2cc81801c8
Zachary Norman <zachary.norman@nv5.com>
2024-05-20 13:40:31 -0600

    Track test time

--- apps/vscode-e2e-runner/src/main.ts
+++ apps/vscode-e2e-runner/src/main.ts
@@ -1,7 +1,11 @@
 import { runTests } from '@vscode/test-electron';
 import { join } from 'path';
+import { performance } from 'perf_hooks';
 
 async function go() {
+  /** Get start time */
+  const t0 = performance.now();
+
   try {
     /** Development folder, hover help below in runTests */
     const extensionDevelopmentPath = process.cwd();
@@ -22,7 +26,9 @@ async function go() {
       extensionDevelopmentPath,
       extensionTestsPath,
     });
+    console.log(`Finished running tests in ${performance.now() - t0} ms`);
   } catch (err) {
+    console.log(`Finished running tests in ${performance.now() - t0} ms`);
     console.error('Failed to run tests');
     process.exit(1);
   }
