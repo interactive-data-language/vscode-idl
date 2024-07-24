5cda04c55bdc838a421e8c1390856851732a9172
Zachary Norman <zachary.norman@nv5.com>
2024-07-24 13:53:47 -0600

    Get script working properly

--- apps/idl-docs-parser/src/index.py
+++ apps/idl-docs-parser/src/index.py
@@ -1,5 +1,5 @@
 from llama_index.core import SimpleDirectoryReader, StorageContext, VectorStoreIndex
-from llama_index.core import set_global_tokenizer, load_index_from_storage,
+from llama_index.core import set_global_tokenizer, load_index_from_storage
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.llama_cpp import LlamaCPP
 from llama_index.llms.llama_cpp.llama_utils import (
@@ -17,6 +17,8 @@ https://docs.llamaindex.ai/en/stable/examples/vector_stores/SimpleIndexDemo/
 
 Custom model that runs locally:
 https://docs.llamaindex.ai/en/stable/examples/llm/llama_2_llama_cpp/
+
+This needs to use a GGUF model file and not bin like the example shows
 """
 
 
@@ -46,7 +48,7 @@ def CreateIndex(source, index_id, destination, embed_model=None):
   return index
 
 
-def LoadIndex(path, index_id)
+def LoadIndex(path, index_id, embed_model=None):
   """
   Load an index from disk
 
@@ -56,8 +58,12 @@ def LoadIndex(path, index_id)
   storage_context = StorageContext.from_defaults(persist_dir=path)
 
   # load index
-  return load_index_from_storage(storage_context, index_id=index_id)
+  return load_index_from_storage(storage_context, index_id=index_id, embed_model=embed_model)
 
+
+# folders
+DOCUMENT_DIR = ""
+CACHE_DIR = ""
 
 # ID for our index (from examples)
 INDEX_ID = "vector_index"
@@ -66,20 +72,20 @@ INDEX_ID = "vector_index"
 embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
 # create our index
-index = CreateIndex("./data/paul_graham/", INDEX_ID, "./storage", embed_model=embed_model)
+# index = CreateIndex(DOCUMENT_DIR, INDEX_ID, CACHE_DIR, embed_model=embed_model)
 
 # load our search index from disk
-# index = LoadIndex("./storage", INDEX_ID)
+index = LoadIndex(CACHE_DIR, INDEX_ID, embed_model=embed_model)
 
 # URL for the model we use
-model_url = "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/llama-2-13b-chat.ggmlv3.q4_0.bin"
+model_url = "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q4_0.gguf"
 
 # create our LLM
 llm = LlamaCPP(
     # You can pass in the URL to a GGML model to download it automatically
     model_url=model_url,
     # optionally, you can set the path to a pre-downloaded model instead of model_url
-    model_path=None,
+    # model_path="",
     temperature=0.1,
     max_new_tokens=256,
     # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
@@ -98,5 +104,5 @@ llm = LlamaCPP(
 # set up query engine
 query_engine = index.as_query_engine(llm=llm)
 
-response = query_engine.query("What did the author do growing up?")
+response = query_engine.query("How much training data do I need to create a model?")
 print(response)
