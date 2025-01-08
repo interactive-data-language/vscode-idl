681b9977c8122909ea104bd10af4470c8d45a6bd
Zachary Norman <zachary.norman@nv5.com>
2025-01-08 09:59:07 -0700

    Duplicate environment

--- apps/client-web/src/main.ts
+++ apps/client-web/src/main.ts
@@ -1,1 +1,20 @@
-console.log('Hello World');
+import { MEASUREMENT } from '@idl/usage-metrics';
+import { ExtensionContext } from 'vscode';
+
+import { environment } from './environments/environment';
+
+MEASUREMENT.ID = environment.measurement;
+
+/**
+ * Function that activates our extension
+ */
+export async function activate(ctx: ExtensionContext): Promise<void> {
+  console.log('I have activated!');
+}
+
+/**
+ * Manages stopping/deactivating our extension
+ */
+export async function deactivate(): Promise<void> {
+  console.log('I have deactivated!');
+}
