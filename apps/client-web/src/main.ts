7628c994c5f23b8a825027231ff6d5914f2a6b02
Zachary Norman <zachary.norman@nv5.com>
2025-01-09 18:05:44 -0700

    Get it working

--- apps/client-web/src/main.ts
+++ apps/client-web/src/main.ts
@@ -5,10 +5,25 @@ import { environment } from './environments/environment';
 
 MEASUREMENT.ID = environment.measurement;
 
+console.log('loaded JS!');
+
 /**
  * Function that activates our extension
  */
 export async function activate(ctx: ExtensionContext): Promise<void> {
+  // InitializeTranslation('en');
+
+  // ctx.extensionUri
+
+  // const ext = vscode.extensions.getExtension(EXTENSION_NAME);
+
+  // console.log(ext.extensionPath);
+
+  // const contents = await vscode.workspace.fs.readFile();
+
+  // readFileSync(GetExtensionPath('idl/routines/global.json'), {
+  //   encoding: 'utf-8',
+  // });
   console.log('I have activated!');
 }
 
