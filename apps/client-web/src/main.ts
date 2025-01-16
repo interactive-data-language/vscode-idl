603837c2943c43fb5225421ca9c6ff90bda66420
Zachary Norman <zacharyanorman@gmail.com>
2025-01-15 19:24:05 -0700

    Broken but it works

--- apps/client-web/src/main.ts
+++ apps/client-web/src/main.ts
@@ -1,45 +1,33 @@
 // import './polyfills';
 
-import { InitializeTranslation } from '@idl/translation';
-import { MEASUREMENT } from '@idl/usage-metrics';
-import { InitializeExtensionConfig } from '@idl/vscode/config';
-import { InitializeLogger } from '@idl/vscode/logger';
-import { InitializeTree } from '@idl/vscode/tree-view';
 import { ExtensionContext } from 'vscode';
 
-import { environment } from './environments/environment';
-
-MEASUREMENT.ID = environment.measurement;
+// MEASUREMENT.ID = environment.measurement;
 
 console.log('loaded JS!');
 
 /**
  * Function that activates our extension
  */
-export default function activate(ctx: ExtensionContext) {
+export async function activate(ctx: ExtensionContext) {
   console.log('They want me to activate!');
 
   try {
-    InitializeLogger(() => {
-      // do nothing
-    });
-
-    InitializeTranslation('en');
-
+    // InitializeTranslation('en');
+    // InitializeLogger(() => {
+    //   // do nothing
+    // });
     // // set logging callback for metrics
     // SetUsageMetricLogger((ev, payload) => {
     //   // do nothing because VSCode has a dedicated channel for viewing telemetry
     //   // and logs it for you
     // });
-
     // InitializeUsageMetrics();
-
-    InitializeExtensionConfig(() => {
-      // do nothing on config changes
-    });
-
-    // initialize our tree view
-    InitializeTree(ctx);
+    // await InitializeExtensionConfig(() => {
+    //   // do nothing on config changes
+    // });
+    // // initialize our tree view
+    // InitializeTree(ctx);
   } catch (err) {
     console.log(err);
   }
--- apps/client-web/webpack.config.js
+++ apps/client-web/webpack.config.js
@@ -2,28 +2,23 @@ const { composePlugins, withNx } = require('@nx/webpack');
 const webpack = require('webpack');
 
 // Nx plugins for webpack.
-module.exports = composePlugins(
-  withNx({
-    target: 'web',
-  }),
-  (config) => {
-    // // add polyfills for some node libs
-    // config.resolve.fallback = {
-    //   os: require.resolve('os-browserify/browser'),
-    //   path: require.resolve('path-browserify'),
-    // };
+module.exports = composePlugins(withNx(), (config) => {
+  // // add polyfills for some node libs
+  // config.resolve.fallback = {
+  //   os: require.resolve('os-browserify/browser'),
+  //   path: require.resolve('path-browserify'),
+  // };
 
-    config.externals = {
-      vscode: 'commonjs vscode', // ignored because it doesn't exist
-    };
+  config.externals = {
+    vscode: 'commonjs vscode', // ignored because it doesn't exist
+  };
 
-    // provide a shim for the global `process` variable
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        process: 'process/browser',
-      })
-    );
+  // provide a shim for the global `process` variable
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    })
+  );
 
-    return config;
-  }
-);
+  return config;
+});
