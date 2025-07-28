f630cf8ed090c6bb44e07bd6509d473eecbe90a6
Zachary Norman <zachary.norman@nv5.com>
2025-07-28 17:25:15 -0600

    Tweak completion priority

--- libs/parsing/index/src/lib/auto-complete/completion-sort-priority.interface.ts
+++ libs/parsing/index/src/lib/auto-complete/completion-sort-priority.interface.ts
@@ -2,13 +2,13 @@
  * Priority for sorting so we have nice display
  */
 export const COMPLETION_SORT_PRIORITY = {
-  STRUCTURES: '00',
-  KEYWORDS: '00',
-  VARIABLES: '01',
-  PROPERTIES: '02',
-  ROUTINES: '03',
-  METHODS: '03',
-  CONTROL: '04',
+  STRUCTURES: '10',
+  KEYWORDS: '10',
+  VARIABLES: '20',
+  PROPERTIES: '30',
+  ROUTINES: '40',
+  METHODS: '40',
+  CONTROL: '50',
   SYSTEM_VARIABLES: '90',
   EXECUTIVE_COMMANDS: '90',
 };
