7fb09657fbc42b720e3bc402753bf7d6c71409fa
Zachary Norman <zacharyanorman@gmail.com>
2025-09-29 23:07:39 -0600

    Allow system variables for type names as they are valid structures

--- libs/tokenizer/tokenizer/src/lib/tokens/defs/types.type-variable.interface.ts
+++ libs/tokenizer/tokenizer/src/lib/tokens/defs/types.type-variable.interface.ts
@@ -8,5 +8,5 @@ export type TypeVariableTokenDef = ITokenDef<TypeVariableToken>;
 
 export const TYPE_VARIABLE: TypeVariableTokenDef = {
   name: TOKEN_NAMES.TYPE_VARIABLE,
-  match: /[a-z_][a-z0-9_$]*/im,
+  match: /!?[a-z_][a-z0-9_$]*/im,
 };
