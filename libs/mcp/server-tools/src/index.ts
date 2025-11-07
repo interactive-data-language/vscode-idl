1a2c3f297884235c1b15684219f445341aa03f79
Zachary Norman <zacharyanorman@gmail.com>
2025-11-06 22:52:56 -0700

    Export new routine

--- libs/mcp/server-tools/src/index.ts
+++ libs/mcp/server-tools/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/helpers/track-envi-task-for-mcp-server';
+export * from './lib/helpers/track-server-resource';
 export * from './lib/mcp-tool-registry.class';
 export * from './lib/register-all-mcp-tools';
 export * from './lib/tools/envi/register-mcp-tool-envi-get-task-parameters';
