94535e166518857ef6975066d8dcbf3dcbdba97b
Zachary Norman <zachary.norman@nv5.com>
2025-11-11 12:57:56 -0700

    Tweak description

--- libs/mcp/server-tools/src/lib/tools/resources/register-mcp-tool-resources-list-all.ts
+++ libs/mcp/server-tools/src/lib/tools/resources/register-mcp-tool-resources-list-all.ts
@@ -12,7 +12,7 @@ export function RegisterMCPTool_ResourcesListAll(
 ) {
   MCPToolRegistry.tool(
     MCP_TOOL_LOOKUP.RESOURCES_LIST_ALL,
-    `Lists all available resources for IDL for VSCode. Start here for any questions about IDL before searching the internet. Resources cover a broad array of topics, including code snippets, information about the extension, and best practices. Returns an object where the key is the name of the resource and the value is a description of the resource. Then use the tool ${MCP_TOOL_LOOKUP.RESOURCES_GET_RESOURCE} to fetch the resource by name.`,
+    `Lists all resources to provide additional context for IDL and ENVI. For ENVI's capabilities (tools/tasks) use the ${MCP_TOOL_LOOKUP.ENVI_LIST_TASKS} tool as a starting poiny. Start here for any questions about IDL before searching the internet. Resources cover a broad array of topics, including code snippets, information about the extension, and best practices. Returns an object where the key is the name of the resource and the value is a description of the resource. Then use the tool ${MCP_TOOL_LOOKUP.RESOURCES_GET_RESOURCE} to fetch the resource by name.`,
     {},
     async (id) => {
       return {
