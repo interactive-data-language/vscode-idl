fa6be75feba6d11aca0d4bf82425e25168d517b7
Zachary Norman <zachary.norman@nv5.com>
2025-11-12 12:44:24 -0700

    Add MCP to contribution

---
+++ apps/package-json/src/contributes/contributes-mcp.ts
@@ -0,0 +1,22 @@
+import { EXTENSION_FULL_NAME } from '@idl/shared/extension';
+import { IDL_TRANSLATION } from '@idl/translation';
+
+import { IPackageJSON, IPackageNLS } from '../package.interface';
+
+/**
+ * Updates the package.json file to list our MCP server that we provide
+ */
+export function ProcessMCP(packageJSON: IPackageJSON, nls: IPackageNLS) {
+  // get all of our contribution points
+  const contrib = packageJSON['contributes'];
+
+  /**
+   * Add primary MCP server that we provide
+   */
+  contrib['mcpServerDefinitionProviders'] = [
+    {
+      id: EXTENSION_FULL_NAME,
+      label: IDL_TRANSLATION.packageJSON.displayName,
+    },
+  ];
+}
