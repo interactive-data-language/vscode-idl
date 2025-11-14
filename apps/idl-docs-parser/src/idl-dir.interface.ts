bb20894e61758f1bfe3c6f07f64a984c1f2741e7
Zachary Norman <zachary.norman@nv5.com>
2025-11-14 12:30:30 -0700

    Update docs to IDL 9.2, ENVI 6.2, DL 4.0

--- apps/idl-docs-parser/src/idl-dir.interface.ts
+++ apps/idl-docs-parser/src/idl-dir.interface.ts
@@ -3,7 +3,7 @@ import { dirname } from 'path';
 /**
  * Location of IDL, ENVI, and ENVI Deep learning
  */
-export const IDL_DIR = `C:\\Program Files\\NV5\\ENVI61\\IDL91`;
+export const IDL_DIR = `C:\\Program Files\\NV5\\ENVI62\\IDL92`;
 
 /**
  * Root folder for ENVI's installation
