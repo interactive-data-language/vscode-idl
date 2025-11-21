d152688e8edeaa2820d1e3569ca5459827bbac1e
Zachary Norman <zacharyanorman@gmail.com>
2025-11-20 22:23:12 -0700

    Clean up unused routine

--- libs/mcp/language-server/src/index.ts
+++ libs/mcp/language-server/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/globals/register-mcp-tool-resources-search-for-routine';
-export * from './lib/globals/track-idl-envi-globals-as-mcp-resources';
 export * from './lib/resources/mcp-track-tutorials-as-resources';
 export * from './lib/tasks/register-mcp-task-tools';
--- libs/mcp/server-tools/src/index.ts
+++ libs/mcp/server-tools/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/helpers/track-envi-task-for-mcp-server';
-export * from './lib/helpers/track-server-resource';
 export * from './lib/mcp-tool-registry.class';
 export * from './lib/register-all-mcp-tools';
 export * from './lib/tools/envi/register-mcp-tool-envi-get-task-parameters';
