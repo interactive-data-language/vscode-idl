c11bfc4990f03af01103ad0039b835a168344702
Zachary Norman <zachary.norman@nv5.com>
2026-01-19 14:16:11 -0700

    Clean up old code, add comment

--- libs/mcp/server/src/lib/start-mcp-server.ts
+++ libs/mcp/server/src/lib/start-mcp-server.ts
@@ -45,77 +45,15 @@ export function StartMCPServer(
     }
   );
 
+  /**
+   * Start express and turn the server on
+   */
   const MCP_HTTP = new McpHttpServerCore(
     MCP_SERVER,
     port,
     logCallback,
     failCallback
   );
-
-  // /** Create express app */
-  // const app = express();
-
-  // /**
-  //  * handle exceptions
-  //  */
-  // process.on('uncaughtException', (err) => {
-  //   if (err.message.includes('EADDRINUSE')) {
-  //     failCallback(err);
-  //   } else {
-  //     console.error(err);
-  //   }
-  // });
-
-  // // from : https://github.com/modelcontextprotocol/typescript-sdk?tab=readme-ov-file#server-side-compatibility
-
-  // // Store transports for each session type
-  // const transports: { [key: string]: SSEServerTransport } = {};
-
-  // // Legacy SSE endpoint for older clients
-  // app.get('/sse', async (req, res) => {
-  //   // Create SSE transport for legacy clients
-  //   const transport = new SSEServerTransport(MCP_MESSAGE_ENDPOINT, res);
-  //   transports[transport.sessionId] = transport;
-
-  //   res.on('close', () => {
-  //     delete transports[transport.sessionId];
-  //   });
-
-  //   // connect
-  //   await MCP_SERVER.connect(transport);
-
-  //   // https://github.com/nrwl/nx-console/blob/bc58d3a5c5c661e3d1fe00248e160cb19575e7aa/apps/nx-mcp/src/main.ts#L127
-  //   // create interval to keep connection alive
-  //   const keepAliveInterval = setInterval(() => {
-  //     // Check if the connection is still open using the socket's writable state
-  //     if (!res.writableEnded && !res.writableFinished) {
-  //       res.write(':beat\n\n');
-  //     } else {
-  //       // console.log('SSE connection closed, clearing keep-alive interval');
-  //       clearInterval(keepAliveInterval);
-  //     }
-  //   }, MCP_SERVER_CONFIG.KEEP_ALIVE_INTERVAL);
-
-  //   // Clean up interval if the client disconnects
-  //   req.on('close', () => {
-  //     // console.log('SSE connection closed by client');
-  //     clearInterval(keepAliveInterval);
-  //   });
-  // });
-
-  // // Legacy message endpoint for older clients
-  // app.post(MCP_MESSAGE_ENDPOINT, async (req, res) => {
-  //   const sessionId = req.query.sessionId as string;
-  //   const transport = transports[sessionId];
-  //   if (transport) {
-  //     await transport.handlePostMessage(req, res);
-  //   } else {
-  //     res.status(400).send('No transport found for sessionId');
-  //   }
-  // });
-
-  // /** Listen on the port */
-  // app.listen(port);
 
   /** update flag that the server started */
   IS_MCP_SERVER_STARTED = true;
