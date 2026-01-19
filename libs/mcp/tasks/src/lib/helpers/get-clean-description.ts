3c66c8fcf45a0541550c8d1a27838ce6fdcb7500
Zachary Norman <zachary.norman@nv5.com>
2026-01-19 08:39:38 -0700

    Add comment per last week

--- libs/mcp/tasks/src/lib/helpers/get-clean-description.ts
+++ libs/mcp/tasks/src/lib/helpers/get-clean-description.ts
@@ -13,7 +13,17 @@ export function GetCleanDescription(description: string, limit = true) {
   /** Strip out examples since they are code */
   const pos = description.toLowerCase().indexOf('## example');
 
-  /** Get only the descriptions */
+  /**
+   * Get only the descriptions
+   *
+   * 1000 character limit is to try and have a high-level description.
+   *
+   * In mid 2025, when Zach tried a 1:1 MCP tool to ENVI Task, there were warning
+   * logs in VSCode that talked about 1000 character limits, so this was added.
+   *
+   * I'm not sure if it is critical still, but some tasks have a lot of content and
+   * we only need a high-level description
+   */
   return markdownToTxt(
     pos !== -1 ? description.substring(0, pos) : description
   ).substring(0, 1000);
