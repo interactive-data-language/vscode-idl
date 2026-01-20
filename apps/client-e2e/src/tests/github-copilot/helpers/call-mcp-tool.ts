0f932ef0c4ac92b0a21d893cf500f4d5fdec04dd
Zachary Norman <zachary.norman@nv5.com>
2026-01-20 13:33:27 -0700

    Add pause for test flow

--- apps/client-e2e/src/tests/github-copilot/helpers/call-mcp-tool.ts
+++ apps/client-e2e/src/tests/github-copilot/helpers/call-mcp-tool.ts
@@ -1,11 +1,17 @@
+import { Sleep } from '@idl/shared/extension';
 import { MCPToolHTTPResponse, MCPToolParams, MCPTools } from '@idl/types/mcp';
 
+import { DEBUG_PAUSE } from '../../debugging/_shared.interface';
 import { CREATED_CLIENT, MCP_CLIENT } from './create-mcp-client';
 
 /**
  * Calls an MCP tool and returns the response
+ *
+ * Manually waits for our DEBUG_PAUSE constant after the
+ * tool finished running to let objects catch up and
+ * synchronize with the main VSCode window
  */
-export function CallMCPTool<T extends MCPTools>(
+export async function CallMCPTool<T extends MCPTools>(
   tool: T,
   params: MCPToolParams<T>
 ) {
@@ -15,8 +21,14 @@ export function CallMCPTool<T extends MCPTools>(
   }
 
   // call tool and strictly type
-  return MCP_CLIENT.callTool({
+  const result = await (MCP_CLIENT.callTool({
     name: tool,
     arguments: params as any,
-  }) as Promise<MCPToolHTTPResponse<T>>;
+  }) as Promise<MCPToolHTTPResponse<T>>);
+
+  // pause momentarily
+  await Sleep(DEBUG_PAUSE);
+
+  // return result
+  return result;
 }
