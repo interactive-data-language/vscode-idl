fd66c3caac95e613dc8f0fa30d342bc4d647dbb8
Zachary Norman <zachary.norman@nv5.com>
2026-01-23 11:40:00 -0700

    Add new export

--- libs/mcp/language-server/src/index.ts
+++ libs/mcp/language-server/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/globals/register-mcp-tool-resources-get-routine-docs';
+export * from './lib/globals/register-mcp-tool-resources-get-routine-docs.interface';
 export * from './lib/globals/register-mcp-tool-resources-search-for-routine';
 export * from './lib/register-all-language-server-mcp-tools';
 export * from './lib/resources/mcp-track-tutorials-as-resources';
