37c73c62cdcb7184a367b1c0cd6f853946113eaa
Zachary Norman <zachary.norman@nv5.com>
2026-01-28 16:47:43 -0700

    WIP: Check shorthand error messages when running MCP tool

--- libs/vscode/debug/src/lib/helpers/handle-envi-success.ts
+++ libs/vscode/debug/src/lib/helpers/handle-envi-success.ts
@@ -1,11 +1,11 @@
 import { CleanIDLOutput } from '@idl/idl/idl-interaction-manager';
-import { IDL_TRANSLATION } from '@idl/translation';
 import { IENVISuccess } from '@idl/types/vscode-debug';
 import { IDL_LOGGER } from '@idl/vscode/logger';
 import { OutputEvent } from '@vscode/debugadapter';
 import * as vscode from 'vscode';
 
 import { IDL_DEBUG_ADAPTER } from '../initialize-debugger';
+import { PopulateENVIError } from './populate-envi-error';
 
 /**
  * Checks an ENVI result for our success message from ENVI
@@ -33,49 +33,14 @@ export async function HandleENVISuccess(
       content: ['Error running in ENVI', parsed, resOrig],
     });
 
-    // determine the error to tell the user
-    let msg = IDL_TRANSLATION.envi.open.defaultError;
-
-    /**
-     * Handle our failure cases
-     */
-    switch (parsed.reason) {
-      case 'envi-error':
-        msg = IDL_TRANSLATION.envi.open.enviError;
-        break;
-      case 'envi-not-started':
-        msg = IDL_TRANSLATION.envi.task.enviNotStarted;
-        break;
-      case 'no-envi-ui':
-        msg = IDL_TRANSLATION.envi.open.noUI;
-        break;
-      case 'no-license':
-        msg = IDL_TRANSLATION.envi.open.noLicense;
-        break;
-      case 'open-error':
-        msg = IDL_TRANSLATION.envi.open.openError;
-        break;
-      case 'task-error':
-        msg = IDL_TRANSLATION.envi.task.unknownTask;
-        break;
-      case 'task-execute-error':
-        msg = IDL_TRANSLATION.envi.task.unknownParam;
-        break;
-      case 'task-param-error':
-        msg = IDL_TRANSLATION.envi.task.unknownParam;
-        break;
-      // do nothing
-      default:
-        break;
-    }
+    // map reported error to human string
+    PopulateENVIError(parsed);
 
     // send reason to IDL console
-    IDL_DEBUG_ADAPTER.sendEvent(
-      new OutputEvent(`${msg}:\n${parsed.error}`, 'stderr')
-    );
+    IDL_DEBUG_ADAPTER.sendEvent(new OutputEvent(`${parsed.error}`, 'stderr'));
 
     // alert user
-    vscode.window.showErrorMessage(msg);
+    vscode.window.showErrorMessage(parsed.error);
   }
 
   return parsed;
