6c3f5359a89812fadc450b299de240c319eda7a7
Zachary Norman <zacharyanorman@gmail.com>
2026-02-03 20:32:09 -0700

    Update port

--- libs/mcp/server/src/lib/mcp-server.interface.ts
+++ libs/mcp/server/src/lib/mcp-server.interface.ts
@@ -7,7 +7,7 @@ export const MCP_MESSAGE_ENDPOINT = '/messages';
  * Default port for our docs server
  */
 export const MCP_SERVER_CONFIG = {
-  /** Port we serve docs from */
+  /** Port for the MCP HTTP server */
   PORT: 4142,
   /** Milliseconds to keep connections alive */
   KEEP_ALIVE_INTERVAL: 30000,
