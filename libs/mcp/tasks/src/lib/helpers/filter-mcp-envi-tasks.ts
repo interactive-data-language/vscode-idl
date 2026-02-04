30b764e747cb1bd01b0490109df7c51b2467943e
Zachary Norman <zachary.norman@nv5.com>
2026-02-04 08:48:07 -0700

    Sort names

--- libs/mcp/tasks/src/lib/helpers/filter-mcp-envi-tasks.ts
+++ libs/mcp/tasks/src/lib/helpers/filter-mcp-envi-tasks.ts
@@ -18,49 +18,51 @@ export function FilterMCPENVITasks(
    * Task names start with "envi" and end with "task" so we have to filter with
    * that assumption in mind
    */
-  return taskNames.filter((className) => {
-    /** Flag if we keep or not */
-    let flag = false;
+  return taskNames
+    .filter((className) => {
+      /** Flag if we keep or not */
+      let flag = false;
 
-    /** Skip non-envi tasks */
-    if (
-      !(className.startsWith('envi') && className.endsWith('task')) ||
-      className === 'envitask'
-    ) {
-      return flag;
-    }
+      /** Skip non-envi tasks */
+      if (
+        !(className.startsWith('envi') && className.endsWith('task')) ||
+        className === 'envitask'
+      ) {
+        return flag;
+      }
 
-    // filter if we don't have a known function
-    if (!(className in functions)) {
-      return false;
-    }
+      // filter if we don't have a known function
+      if (!(className in functions)) {
+        return false;
+      }
 
-    /** Get task name */
-    const taskName = TASK_REGEX.exec(className)[1];
+      /** Get task name */
+      const taskName = TASK_REGEX.exec(className)[1];
 
-    /**
-     * Cases that we return from
-     */
-    switch (true) {
       /**
-       * Exclude any of the extract routines which we dont need
-       *
-       * Dehydrated parameters should account for these instead or dedicated
-       * MCP tools
-       *
-       * This is code and not a static list so it is future-proof
+       * Cases that we return from
        */
-      case taskName.startsWith('extract') && taskName.endsWith('fromfile'):
-        break;
-      // skip tasks with names we decide not to expose as tools
-      case taskName in SKIP_THESE_TASKS:
-        break;
-      // otherwise we keep it
-      default:
-        flag = true;
-        break;
-    }
+      switch (true) {
+        /**
+         * Exclude any of the extract routines which we dont need
+         *
+         * Dehydrated parameters should account for these instead or dedicated
+         * MCP tools
+         *
+         * This is code and not a static list so it is future-proof
+         */
+        case taskName.startsWith('extract') && taskName.endsWith('fromfile'):
+          break;
+        // skip tasks with names we decide not to expose as tools
+        case taskName in SKIP_THESE_TASKS:
+          break;
+        // otherwise we keep it
+        default:
+          flag = true;
+          break;
+      }
 
-    return flag;
-  });
+      return flag;
+    })
+    .sort();
 }
