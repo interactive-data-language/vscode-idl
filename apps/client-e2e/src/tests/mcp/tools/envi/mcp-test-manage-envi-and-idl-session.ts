210d94c942e59d0b62b701ad9dcd5fda1ae1c0c2
Zachary Norman <zachary.norman@nv5.com>
2026-02-05 10:06:20 -0700

    Tweak a bit

--- apps/client-e2e/src/tests/mcp/tools/envi/mcp-test-manage-envi-and-idl-session.ts
+++ apps/client-e2e/src/tests/mcp/tools/envi/mcp-test-manage-envi-and-idl-session.ts
@@ -6,6 +6,7 @@ import expect from 'expect';
 import { DEBUG_PAUSE } from '../../../debugging/_shared.interface';
 import { RunnerFunction } from '../../../runner.interface';
 import { CallMCPTool } from '../../helpers/call-mcp-tool';
+import { LogWhenExpectSuccess } from '../../helpers/test-loggers';
 
 /**
  * Makes sure we can manage our session
@@ -24,13 +25,15 @@ export const RunMCPManageENVIAndIDLSession: RunnerFunction = async (init) => {
    * Restart IDL
    * =========================================================
    */
-  expect(
-    (
-      await CallMCPTool(MCP_TOOL_LOOKUP.MANAGE_ENVI_AND_IDL_SESSION, {
-        action: 'restart-idl',
-      })
-    ).isError
-  ).toBeFalsy();
+  console.log(`  Restarting IDL`);
+  const respRestartIDL = await CallMCPTool(
+    MCP_TOOL_LOOKUP.MANAGE_ENVI_AND_IDL_SESSION,
+    {
+      action: 'restart-idl',
+    }
+  );
+  LogWhenExpectSuccess(respRestartIDL);
+  expect(respRestartIDL.isError).toBeFalsy();
 
   // make sure launched
   expect(init.debug.adapter.isStarted()).toBeTruthy();
@@ -40,14 +43,15 @@ export const RunMCPManageENVIAndIDLSession: RunnerFunction = async (init) => {
    * Restart ENVI with headless
    * =========================================================
    */
-
-  expect(
-    (
-      await CallMCPTool(MCP_TOOL_LOOKUP.MANAGE_ENVI_AND_IDL_SESSION, {
-        action: 'restart-envi-headless',
-      })
-    ).isError
-  ).toBeFalsy();
+  console.log(`  Restarting ENVI headlessly`);
+  const respRestartENVIHeadless = await CallMCPTool(
+    MCP_TOOL_LOOKUP.MANAGE_ENVI_AND_IDL_SESSION,
+    {
+      action: 'restart-envi-headless',
+    }
+  );
+  LogWhenExpectSuccess(respRestartENVIHeadless);
+  expect(respRestartENVIHeadless.isError).toBeFalsy();
 
   // make sure launched
   expect(init.debug.adapter.isStarted()).toBeTruthy();
@@ -65,14 +69,15 @@ export const RunMCPManageENVIAndIDLSession: RunnerFunction = async (init) => {
    * Restart ENVI with UI
    * =========================================================
    */
-
-  expect(
-    (
-      await CallMCPTool(MCP_TOOL_LOOKUP.MANAGE_ENVI_AND_IDL_SESSION, {
-        action: 'restart-envi-ui',
-      })
-    ).isError
-  ).toBeFalsy();
+  console.log(`  Restarting ENVI with UI`);
+  const respRestartENVIU = await CallMCPTool(
+    MCP_TOOL_LOOKUP.MANAGE_ENVI_AND_IDL_SESSION,
+    {
+      action: 'restart-envi-ui',
+    }
+  );
+  LogWhenExpectSuccess(respRestartENVIU);
+  expect(respRestartENVIU.isError).toBeFalsy();
 
   // verify we started
   expect(init.debug.adapter.isStarted()).toBeTruthy();
@@ -87,14 +92,15 @@ export const RunMCPManageENVIAndIDLSession: RunnerFunction = async (init) => {
    * Stop works OK
    * =========================================================
    */
-
-  expect(
-    (
-      await CallMCPTool(MCP_TOOL_LOOKUP.MANAGE_ENVI_AND_IDL_SESSION, {
-        action: 'stop',
-      })
-    ).isError
-  ).toBeFalsy();
+  console.log(`  Stopping`);
+  const respStop = await CallMCPTool(
+    MCP_TOOL_LOOKUP.MANAGE_ENVI_AND_IDL_SESSION,
+    {
+      action: 'stop',
+    }
+  );
+  LogWhenExpectSuccess(respStop);
+  expect(respStop.isError).toBeFalsy();
 
   // pause while stopping
   await Sleep(DEBUG_PAUSE);
