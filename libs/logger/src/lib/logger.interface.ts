c54b2a13130fbff96c09ba6cbd15a7d9c3a37ffa
Zachary Norman <zacharyanorman@gmail.com>
2026-03-03 23:22:38 -0700

    [NPM INSTALL] More udpates

--- libs/logger/src/lib/logger.interface.ts
+++ libs/logger/src/lib/logger.interface.ts
@@ -1,7 +1,7 @@
-import * as chalk from 'chalk';
+import chalk from 'chalk';
 
 // force chalk to be enabled
-(chalk as any).level = 2;
+chalk.level = 2;
 
 import { Logger } from './logger.class';
 
