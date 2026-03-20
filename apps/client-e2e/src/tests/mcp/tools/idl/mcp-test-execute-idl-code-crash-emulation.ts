f90b24774d4ac3be8d47ec0c6606743c7dda4d31
Zachary Norman <zachary.norman@nv5.com>
2026-03-20 14:29:09 -0600

    WIP: New MCP test

---
+++ apps/client-e2e/src/tests/mcp/tools/idl/mcp-test-execute-idl-code-crash-emulation.ts
@@ -0,0 +1,37 @@
+import { MCP_TOOL_LOOKUP } from '@idl/types/mcp';
+import expect from 'expect';
+
+import { RunnerFunction } from '../../../runner.interface';
+import { CallMCPTool } from '../../helpers/call-mcp-tool';
+
+/**
+ * Makes sure we can start IDL through MCP
+ */
+export const RunMCPTestExecuteIDLCode_CrashEmulation: RunnerFunction = async (
+  init
+) => {
+  /**
+   * Run code that completes
+   */
+  const result = await CallMCPTool(MCP_TOOL_LOOKUP.EXECUTE_IDL_CODE, {
+    code: `print, 'foo'`,
+  });
+
+  // make sure the tool runs
+  expect(result.isError).toBeFalsy();
+
+  // verify we started
+  expect(init.debug.adapter.isStarted()).toBeTruthy();
+
+  /**
+   * Run code that causes IDL to crash/exit
+   */
+  const runtimeError = await CallMCPTool(MCP_TOOL_LOOKUP.EXECUTE_IDL_CODE, {
+    code: `help, 42\nexit`,
+  });
+
+  expect(runtimeError.isError).toBeTruthy();
+
+  // verify we stopped
+  expect(init.debug.adapter.isStarted()).toBeFalsy();
+};
