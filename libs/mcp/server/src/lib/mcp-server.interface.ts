c70a7c2e41ef17b5fb3ecc2243ad14c961f17c57
Zachary Norman <zachary.norman@nv5.com>
2026-03-31 16:29:21 -0600

    Remove message endpoint

--- libs/mcp/server/src/lib/mcp-server.interface.ts
+++ libs/mcp/server/src/lib/mcp-server.interface.ts
@@ -77,11 +77,6 @@ export interface IMCPConnection {
 }
 
 /**
- * URL base for messages
- */
-export const MCP_MESSAGE_ENDPOINT = '/messages';
-
-/**
  * Default port for our docs server
  */
 export const MCP_SERVER_CONFIG = {
