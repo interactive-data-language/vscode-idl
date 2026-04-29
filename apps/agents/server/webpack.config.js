f9442cf30c8614362270bfe0d520272c3add4a58
Zachary Norman <zachary.norman@nv5.com>
2026-04-28 20:55:56 -0600

    Add zod webpack config

--- apps/agents/server/webpack.config.js
+++ apps/agents/server/webpack.config.js
@@ -1,4 +1,5 @@
 const { composePlugins, withNx } = require('@nx/webpack');
+const path = require('path');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(
@@ -14,6 +15,15 @@ module.exports = composePlugins(
       }),
     };
     config.devtool = 'source-map';
+
+    if (!config.resolve) {
+      config.resolve = {};
+    }
+    if (!config.resolve.alias) {
+      config.resolve.alias = {};
+    }
+    config.resolve.alias.zod$ = path.resolve(__dirname, 'node_modules/zod/v3');
+
     // Update the webpack config as needed here.
     // e.g. `config.plugins.push(new MyPlugin())`
     return config;
