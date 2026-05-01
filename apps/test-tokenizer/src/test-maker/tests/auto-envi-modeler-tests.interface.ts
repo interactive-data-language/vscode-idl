e263bb106f0d9357d2f8875b671b2c1f752d32bb
Zachary Norman <zachary.norman@nv5.com>
2026-05-01 16:24:42 -0600

    Add new ENVI Modeler test

--- apps/test-tokenizer/src/test-maker/tests/auto-envi-modeler-tests.interface.ts
+++ apps/test-tokenizer/src/test-maker/tests/auto-envi-modeler-tests.interface.ts
@@ -15,6 +15,7 @@ import { AUTO_ENVI_MODELER_TEST_ORTHORECTIFICATION } from './envi-modeler-workfl
 import { AUTO_ENVI_MODELER_TEST_PARAMS_BAD } from './envi-modeler-workflows/params-bad';
 import { AUTO_ENVI_MODELER_TEST_SELECTIVE_DOWNSAMPLING } from './envi-modeler-workflows/selective-downsampling';
 import { AUTO_ENVI_MODELER_TEST_SPECTRAL_INDEX_ISODATA } from './envi-modeler-workflows/spectral-index-isodata';
+import { AUTO_ENVI_MODELER_TEST_STATIC_INPUT } from './envi-modeler-workflows/static-input';
 
 /**
  * Automated tests for ENVI Modeler validation and workflow creation.
@@ -39,4 +40,5 @@ export const AUTO_ENVI_MODELER_TESTS: IAutoENVIModelerTest[] = [
   AUTO_ENVI_MODELER_TEST_PARAMS_BAD,
   AUTO_ENVI_MODELER_TEST_SELECTIVE_DOWNSAMPLING,
   AUTO_ENVI_MODELER_TEST_SPECTRAL_INDEX_ISODATA,
+  AUTO_ENVI_MODELER_TEST_STATIC_INPUT,
 ];
