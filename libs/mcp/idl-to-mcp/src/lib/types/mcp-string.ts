cd5a8181c8edbb7542476b1c84182248c314f4d5
Zachary Norman <zachary.norman@nv5.com>
2026-05-22 08:58:48 -0600

    Fix type error

--- libs/mcp/idl-to-mcp/src/lib/types/mcp-string.ts
+++ libs/mcp/idl-to-mcp/src/lib/types/mcp-string.ts
@@ -5,7 +5,7 @@ import { z } from 'zod';
  */
 export function MCP_String(values?: string[]) {
   // check for values from a choice list and map to literals
-  if (values?.length > 0) {
+  if (Array.isArray(values)) {
     return z.union(values.map((v) => z.literal(v)) as any);
   } else {
     return z.string();
