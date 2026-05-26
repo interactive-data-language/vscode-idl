8007721234583988e4f08c82688f0cb6360eea1d
Zachary Norman <zachary.norman@nv5.com>
2026-05-26 15:54:25 -0600

    Fix imports in tests

--- apps/test-tokenizer/src/test-maker/makers/tests-for-task-generation.ts
+++ apps/test-tokenizer/src/test-maker/makers/tests-for-task-generation.ts
@@ -35,9 +35,7 @@ export async function TestsForTaskGeneration(
   strings.push(
     `import { GenerateIDLTask, GenerateIDLTaskMainLevelProgram, } from '@idl/generators/idl-task';`,
   );
-  strings.push(
-    `import { GenerateTaskResult } from '@idl/generators/tasks-shared';`,
-  );
+  strings.push(`import { GenerateTaskResult } from '@idl/types/generators';`);
   strings.push(`import { GetExtensionPath } from '@idl/idl/files';`);
   strings.push(`import { LogManager } from '@idl/logger';`);
   strings.push(
