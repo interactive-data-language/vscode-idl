b649275f39aa85a31b045a077b37fbea7f2add4b
Zachary Norman <zachary.norman@nv5.com>
2026-08-28 09:49:45 -0600

    Fix export

--- libs/types/electron/src/index.ts
+++ libs/types/electron/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/electron-config.interface';
+export * from './lib/electron-events.interface';
 export * from './lib/model-config.interface';
 export * from './lib/types-electron';
--- libs/types/electron/src/lib/electron-events.interface.ts
+++ libs/types/electron/src/lib/electron-events.interface.ts
@@ -4,5 +4,5 @@
 export const ELECTRON_EVENTS = {
   CONFIG_CHANGED: 'config-changed',
   GET_CONFIG: 'get-config',
-  SET_CONFIG: 'set-config'
-}
+  SET_CONFIG: 'set-config',
+};
