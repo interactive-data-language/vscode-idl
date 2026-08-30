a88fccfae5a5c173832e78a512d3dfab29500d06
Zachary Norman <zachary.norman@nv5.com>
2026-08-30 05:58:45 -0600

    Fix task display name bug when globals not loaded

--- libs/parsing/type-parser/src/lib/helpers/get-task-display-name.ts
+++ libs/parsing/type-parser/src/lib/helpers/get-task-display-name.ts
@@ -4,7 +4,22 @@ import { TASK_REGEX } from '../parsing/parse-idl-type.interface';
  * Get task display name information
  */
 export function GetTaskDisplayName(baseType: string) {
-  const taskName = TASK_REGEX.exec(baseType)[1];
+  /**
+   * Init task name value
+   *
+   * This handles when our baseType = "ENVITask" or "IDLTask"
+   *
+   * Which can happen when we don't have global tokens loaded
+   */
+  let taskName = 'any';
+
+  // try to extract
+  const match = TASK_REGEX.exec(baseType);
+  if (match !== null) {
+    taskName = match[0];
+  }
+
+  // populate and return
   if (baseType.toLowerCase().startsWith('envi')) {
     return {
       type: 'ENVITask',
