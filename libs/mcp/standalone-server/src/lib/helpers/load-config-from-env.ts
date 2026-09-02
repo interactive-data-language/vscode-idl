95188011ee7ff1e213003ac4c9b597c48907dd16
Zachary Norman <zachary.norman@nv5.com>
2026-09-02 04:57:45 -0600

    Tweak env logic

--- libs/mcp/standalone-server/src/lib/helpers/load-config-from-env.ts
+++ libs/mcp/standalone-server/src/lib/helpers/load-config-from-env.ts
@@ -4,7 +4,7 @@ import { IAgentServerConfig, OpenAIModelConfig } from '@idl/types/agents';
  * Check what configuration options should be set dynamically
  */
 export function LoadConfigFromEnv(config: IAgentServerConfig) {
-  if (process.env.OPENAI_API_KEY) {
+  if (process.env.OPENAI_API_KEY && config.agent.llm.model === 'openai') {
     console.log('[config] Found OpenAI API Key, overriding server config');
     config.agent.llm.model = 'openai';
     (config.agent.llm.config as OpenAIModelConfig).apiKey =
